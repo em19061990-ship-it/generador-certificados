feba8ce4ff9051f78f2536d21f31d96ba4cff85f
em19061990-ship-it <em19061990@gmail.com>
2026-02-23 19:48:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import os
 
 # Configuración de la interfaz de la aplicación
-st.set_page_config(page_title="Generador de Certificados SCA", page_icon="☕")
+st.set_page_config(page_title="Generador de Diploma Barista", page_icon="☕")
 
 class CertificatePDF(FPDF):
     def add_background(self):
@@ -91,8 +91,8 @@ def generate_certificate(name, date_obj):
     return pdf.output(dest='S').encode('latin-1')
 
 # --- INTERFAZ DE USUARIO EN STREAMLIT ---
-st.title("☕ Generador de Certificados SCA")
-st.write("Emisión de diplomas profesionales con fondo personalizado.")
+st.title("☕ Generador de Diploma Barista")
+st.write("Gracias por estar en el curso de Baristas Dulce Fénix.")
 
 with st.form("form_emision"):
     nombre_alumno = st.text_input("Nombre completo del Alumno", placeholder="Ej. Dulce Fénix")
